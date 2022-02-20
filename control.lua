ce9000141bef0e0d3300979bb9ef9a1aabbc514e
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2022-02-20 14:45:55 +0100

    The bundeling of the Lua code seems to break factorio map load

--- control.lua
+++ control.lua
@@ -1,27 +1,4 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-
-local ____modules = {}
-local ____moduleCache = {}
-local ____originalRequire = require
-local function require(file, ...)
-    if ____moduleCache[file] then
-        return ____moduleCache[file].value
-    end
-    if ____modules[file] then
-        local module = ____modules[file]
-        ____moduleCache[file] = { value = (select("#", ...) > 0) and module(...) or module(file) }
-        return ____moduleCache[file].value
-    else
-        if ____originalRequire then
-            return ____originalRequire(file)
-        else
-            error("module '" .. file .. "' not found")
-        end
-    end
-end
-____modules = {
-["control"] = function(...) 
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 script.on_event(
     defines.events.on_tick,
     function(_evt)
@@ -41,6 +18,3 @@ script.on_event(
         main_frame.auto_center = true
     end
 )
- end,
-}
-return require("control", ...)
